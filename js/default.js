085aebeeabd6b9c53649bcf3f9a330c08b4b311f
liyanguestc <liyanguestc@gmail.com>
2014-12-25 09:33:25 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -11,9 +11,10 @@ var earthMesh;
 function Init(){
         scene = new THREE.Scene();
        //setup camera
- 		camera = new LeiaCamera();
-        camera.position.copy(new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z));
-        camera.lookAt(new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z));
+        camera = new LeiaCamera({
+           cameraPosition:new THREE.Vector3(_camPosition.x, _camPosition.y, _camPosition.z), 
+		   targetPosition: new THREE.Vector3(_tarPosition.x, _tarPosition.y, _tarPosition.z)
+        });
         scene.add(camera);
   
        //setup rendering parameter
@@ -21,6 +22,7 @@ function Init(){
          antialias:true, 
  		renderMode: _renderMode, 
 		shaderMode: _nShaderMode,
+		colorMode : _colorMode,
 		devicePixelRatio: 1 
         } );
  		renderer.Leia_setSize( windowWidth, windowHeight );
@@ -43,7 +45,15 @@ function Init(){
  	requestAnimationFrame( animate );
     renderer.setClearColor(new THREE.Color().setRGB(0.0, 0.0, 0.0)); 
    earthMesh.rotation.y  += 1/32 * delta;
-	renderer.Leia_render(scene, camera,undefined,undefined,_holoScreenSize,_camFov,_messageFlag);
+	renderer.Leia_render({
+     scene:scene, 
+     camera:camera,
+     holoScreenSize:_holoScreenSize,
+     holoCamFov:_camFov,
+      upclip: _up,
+     downclip:  _down,
+     messageFlag:_messageFlag
+   });
  }
 
 function addObjectsToScene(){
